279bf756e69808f47a8d6f5f50f32c1e8bb81a04
svenchilton <sven.chilton@gmail.com>
2021-08-09 16:53:01 -0700

    Modified the app to use the lvwerra/bert-imdb model from Hugging Face. Updated the virtual environment requirements.

--- sentiment_analyzer/api.py
+++ sentiment_analyzer/api.py
@@ -1,7 +1,6 @@
-from typing import Dict
-
 from fastapi import Depends, FastAPI
 from pydantic import BaseModel
+from typing import Dict, List, Union
 
 from .classifier.model import Model, get_model
 
@@ -9,18 +8,23 @@ app = FastAPI()
 
 
 class SentimentRequest(BaseModel):
-    text: str
+    text: Union[str, List[str]]
 
 
 class SentimentResponse(BaseModel):
-    probabilities: Dict[str, float]
-    sentiment: str
-    confidence: float
+    results: List[Dict[str, Union[str, float]]]
 
 
 @app.post("/predict", response_model=SentimentResponse)
 def predict(request: SentimentRequest, model: Model = Depends(get_model)):
-    sentiment, confidence, probabilities = model.predict(request.text)
-    return SentimentResponse(
-        sentiment=sentiment, confidence=confidence, probabilities=probabilities
-    )
+    # When passing the path to a file into a field of an httpie request 
+    # with the =@ method, httpie reads in the contents of that file as a 
+    # single string. In that case, we'll convert that into a list of 
+    # strings by splitting on the newline character \n.
+    text = request.text
+    # Note that this will also convert single strings without \n to 
+    # single-element lists of strings
+    if type(text) is not list:
+        text = text.split('\n')
+    results = model.predict(text)
+    return SentimentResponse(results=results)
